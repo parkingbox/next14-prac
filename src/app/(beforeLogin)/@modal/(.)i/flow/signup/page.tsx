6ad4c12b55926e51e31043b2abdc43c78ddae12a
박승우 <qpfgud430@gmail.com>
2024-01-27 19:11:14 +0900

    Parallel Routes

---
+++ src/app/(beforeLogin)/@modal/(.)i/flow/signup/page.tsx
@@ -0,0 +1,10 @@
+import SignupModal from "@/app/(beforeLogin)/_component/SignUpModal";
+
+export default function Signup() {
+  return (
+    <>
+      난 가로채기지롱 ㅋㅋ
+      <SignupModal />
+    </>
+  );
+}
